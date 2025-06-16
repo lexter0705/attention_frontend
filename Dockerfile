b19173dd88d564538b4f4d95b16b98283c3ba62d
Alexey <88457801+lexter0705@users.noreply.github.com>
2025-06-16 03:28:27 +0300

    Some

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM node:20-alpine
 
-COPY . .
+COPY package-lock.json .
+
+COPY package-lock.json .
 
 RUN npm i
+
+COPY . .
 
 CMD ["npm", "start"]
--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
-	apiBaseURL: "http://localhost:8000",
+	apiBaseURL: "http://207.180.227.75:8000",
 	loginEndpoint: "users/login",
 	registration: "users/register",
-	videoWS: "ws://localhost:8000/websocket/connect_user",
+	videoWS: "ws://207.180.227.75:8000/websocket/connect_user",
 	canvasWidth: "640",
 	canvasHeight: "480",
 }
